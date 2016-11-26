5963725af95f1352c8d667d86372626616fbca04
Vikram Deshmukh <srvikram13@gmail.com>
2016-11-26 14:01:56 -0800

    first commit..

---
+++ WhatsForDinner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsForDinner
+//
+//  Created by Vikram Deshmukh on 11/18/16.
+//  Copyright © 2016 Vikram Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
